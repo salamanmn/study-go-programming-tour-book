cb33dbbdf333d9b8e206ef1164ac10bc0e1779c4
wanghao <1804981203@qq.com>
2024-04-12 17:51:13 +0800

    feat:完成公共组件之日志写入

--- blog-service/go.mod
+++ blog-service/go.mod
@@ -51,6 +51,7 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.6 // indirect
 )
--- blog-service/main.go
+++ blog-service/main.go
@@ -9,18 +9,27 @@ import (
 	"github.com/go-programming-tour-book/blog-service/global"
 	"github.com/go-programming-tour-book/blog-service/internal/model"
 	"github.com/go-programming-tour-book/blog-service/internal/routers"
+	"github.com/go-programming-tour-book/blog-service/pkg/logger"
 	"github.com/go-programming-tour-book/blog-service/pkg/setting"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func init() {
+
 	err := setupSetting()
 	if err != nil {
 		log.Fatalf("init.setupSetting err: %v", err)
 	}
-	err = setupDBEngine()
+
+	err = setupLogger()
 	if err != nil {
-		log.Fatalf("init.setupDBEngine err: %v", err)
+		log.Fatalf("init.setupLogger err: %v", err)
 	}
+
+	// err = setupDBEngine()
+	// if err != nil {
+	// 	log.Fatalf("init.setupDBEngine err: %v", err)
+	// }
 }
 
 func setupSetting() error {
@@ -47,11 +56,23 @@ func setupSetting() error {
 
 func setupDBEngine() error {
 	var err error
-	
-	global.DBEngine, err =  model.NewDBEngine(global.DatabaseSetting)
+
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func setupLogger() error {
+	fileName := global.AppSetting.LogSavePath + "/" + global.AppSetting.LogFileName + global.AppSetting.LogFileExt
+	global.Logger = logger.NewLogger(&lumberjack.Logger{
+		Filename:  fileName,
+		MaxSize:   500,
+		MaxAge:    10,
+		LocalTime: true,
+	}, "", log.LstdFlags).WithCaller(2)
 
 	return nil
 }
